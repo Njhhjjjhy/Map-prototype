f6ac0e636061245bf0d857ae13fbee2fa09a8d3f
RiaanMOHA <riaan.burger@moreharvest.com>
2026-03-13 16:03:31 +0800

    Remove station icons from Airport access toggle
    When "Airport access" is toggled on in the dashboard, do not show the "New Kikuyo Station" or "Higo-Ozu Station" icons

--- js/map/infrastructure.js
+++ js/map/infrastructure.js
@@ -1255,7 +1255,9 @@ export const methods = {
       microscope: `<svg viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" width="14" height="14"><path d="M6 18h8"/><path d="M3 22h18"/><path d="M14 22a7 7 0 1 0 0-14h-1"/><path d="M9 14h2"/><path d="M9 12a2 2 0 0 1-2-2V6h6v4a2 2 0 0 1-2 2Z"/><path d="M12 6V3a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v3"/></svg>`,
     };
 
-    data.landmarks.forEach((lm) => {
+    // Skip station landmarks - these belong to "New railway stations" toggle
+    const stationLandmarkIds = ["higo-ozu", "new-kikuyo-landmark"];
+    data.landmarks.filter((lm) => !stationLandmarkIds.includes(lm.id)).forEach((lm) => {
       const id = `ga-landmark-${lm.id}`;
 
       // Use branded JASM logo instead of generic icon
